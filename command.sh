ff123c0986886d40bb01821c167aadee57190781
kriteshshah <kriteshshah201@gmail.com>
2026-03-17 16:01:23 +0530

    Remove cron

--- command.sh
+++ command.sh
@@ -3,9 +3,8 @@
 set -e
 
 #  python manage.py loaddata permissions asset_categories fields roles &&
-python manage.py crontab add &&
+
 python manage.py migrate &&
 python manage.py collectstatic --noinput &&
-service cron start &&
 
 gunicorn chatproject.wsgi:application --bind 0.0.0.0:8000 --timeout 600
